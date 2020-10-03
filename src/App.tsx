1bb011315fe64d699c441919533f1bc7d9559442
jacekk <github.com@jacek-k.net>
2020-10-03 02:21:39 +0200

    added beautiful-react-hooks@0.30.0 and used; removed testing setup;

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useGlobalEvent } from 'beautiful-react-hooks';
 
 function App() {
+	const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+	const onWindowResize = useGlobalEvent('resize');
+
+	onWindowResize(() => {
+		setWindowWidth(window.innerWidth);
+	});
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<pre>{JSON.stringify({ windowWidth }, null, 4)}</pre>
 		</div>
 	);
 }
